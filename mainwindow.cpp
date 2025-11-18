05c59084368320dc4f31a9f57af6407d8ee88fa4
Geo6453 <geologz2064@gmail.com>
2025-11-18 11:09:11 +0100

    add disable button's feature

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,29 +14,45 @@ MainWindow::MainWindow(QWidget *parent)
         QLabel *choiceGame = new QLabel("Choose game :", this);
         gridLayout->addWidget(choiceGame, 0, 0, 1, 2);
 
-        QPushButton *xc1 = new QPushButton("Xenoblade DE", this);
-        xc1->setFixedWidth(200);
-        gridLayout->addWidget(xc1, 1, 0);
-        connect(xc1, &QPushButton::clicked, this, [this, stack]()
-                {stack->setCurrentIndex(1);});
+        xb1 = new QPushButton("Xenoblade DE", this);
+        xb1->setFixedWidth(200);
+        gridLayout->addWidget(xb1, 1, 0);
+        connect(xb1, &QPushButton::clicked, this, [this, stack]()
+        {
+            commandGame = "xb1";
+            stack->setCurrentIndex(1);
+            GenerateDropTables->setEnabled(true);
+        });
 
-        QPushButton *xc2 = new QPushButton("Xenoblade 2", this);
-        xc2->setFixedWidth(200);
-        gridLayout->addWidget(xc2, 1, 1);
-        connect(xc2, &QPushButton::clicked, this, [this, stack]()
-                {stack->setCurrentIndex(1);});
+        xb2 = new QPushButton("Xenoblade 2", this);
+        xb2->setFixedWidth(200);
+        gridLayout->addWidget(xb2, 1, 1);
+        connect(xb2, &QPushButton::clicked, this, [this, stack]()
+        {
+            commandGame = "xb2";
+            stack->setCurrentIndex(1);
+            CreateBlade->setEnabled(true);
+            ReadSave->setEnabled(true);
+            DecompressIraSave->setEnabled(true);
+        });
 
-        QPushButton *xc3 = new QPushButton("Xenoblade 3", this);
-        xc3->setFixedWidth(200);
-        gridLayout->addWidget(xc3, 2, 0);
-        connect(xc3, &QPushButton::clicked, this, [this, stack]()
-                {stack->setCurrentIndex(1);});
+        xb3 = new QPushButton("Xenoblade 3", this);
+        xb3->setFixedWidth(200);
+        gridLayout->addWidget(xb3, 2, 0);
+        connect(xb3, &QPushButton::clicked, this, [this, stack]()
+        {
+            stack->setCurrentIndex(1);
+            commandGame = "xb3";
+        });
 
-        QPushButton *xcx = new QPushButton("Xenoblade X", this);
-        xcx->setFixedWidth(200);
-        gridLayout->addWidget(xcx, 2, 1);
-        connect(xcx, &QPushButton::clicked, this, [this, stack]()
-                {stack->setCurrentIndex(1);});
+        xbx = new QPushButton("Xenoblade X", this);
+        xbx->setFixedWidth(200);
+        gridLayout->addWidget(xbx, 2, 1);
+        connect(xbx, &QPushButton::clicked, this, [this, stack]()
+        {
+            stack->setCurrentIndex(1);
+            commandGame = "xbx";
+        });
     stack->addWidget(home);
 
     QWidget *task = new QWidget();
@@ -47,78 +63,86 @@ MainWindow::MainWindow(QWidget *parent)
 
         QLabel *archive = new QLabel("Archive", this);
         taskGridLayout->addWidget(archive, 1, 0);
-            QPushButton *ExtractArchive = new QPushButton("Extract Archive", this);
+            ExtractArchive = new QPushButton("Extract Archive", this);
             ExtractArchive->setFixedWidth(200);
             taskGridLayout->addWidget(ExtractArchive, 2, 0);
 
-            QPushButton *ReplaceArchive = new QPushButton("Replace Archive", this);
+            ReplaceArchive = new QPushButton("Replace Archive", this);
             ReplaceArchive->setFixedWidth(200);
             taskGridLayout->addWidget(ReplaceArchive, 3, 0);
 
         QLabel *bdat = new QLabel("BDAT", this);
         taskGridLayout->addWidget(bdat, 1, 1);
-            QPushButton *DecryptBdat = new QPushButton("Decrypt Bdat", this);
+            DecryptBdat = new QPushButton("Decrypt Bdat", this);
             DecryptBdat->setFixedWidth(200);
             taskGridLayout->addWidget(DecryptBdat, 2, 1);
 
-            QPushButton *BdatCodeGen = new QPushButton("Bdat code generator", this);
+            BdatCodeGen = new QPushButton("Bdat code generator", this);
             BdatCodeGen->setFixedWidth(200);
             taskGridLayout->addWidget(BdatCodeGen, 3, 1);
 
-            QPushButton *Bdat2Html = new QPushButton("Bdat to HTML", this);
+            Bdat2Html = new QPushButton("Bdat to HTML", this);
             Bdat2Html->setFixedWidth(200);
             taskGridLayout->addWidget(Bdat2Html, 4, 1);
 
-            QPushButton *Bdat2Json = new QPushButton("Bdat to JSON", this);
+            Bdat2Json = new QPushButton("Bdat to JSON", this);
             Bdat2Json->setFixedWidth(200);
             taskGridLayout->addWidget(Bdat2Json, 5, 1);
 
-            QPushButton *Bdat2Psql = new QPushButton("Bdat to PSQL", this);
+            Bdat2Psql = new QPushButton("Bdat to PSQL", this);
             Bdat2Psql->setFixedWidth(200);
             taskGridLayout->addWidget(Bdat2Psql, 6, 1);
 
-            QPushButton *GenerateData = new QPushButton("Generate Data", this);
+            GenerateData = new QPushButton("Generate Data", this);
             GenerateData->setFixedWidth(200);
             taskGridLayout->addWidget(GenerateData, 7, 1);
 
         QLabel *other = new QLabel("Other", this);
         taskGridLayout->addWidget(other, 1, 2);
-            QPushButton *DescrambleScript = new QPushButton("Descramble Script", this);
+            DescrambleScript = new QPushButton("Descramble Script", this);
             DescrambleScript->setFixedWidth(200);
             taskGridLayout->addWidget(DescrambleScript, 2, 2);
 
-            QPushButton *ExtractWilay = new QPushButton("Extract Wilay", this);
+            ExtractWilay = new QPushButton("Extract Wilay", this);
             ExtractWilay->setFixedWidth(200);
             taskGridLayout->addWidget(ExtractWilay, 3, 2);
 
         QLabel *xb2only = new QLabel("Xenoblade 2 only", this);
         taskGridLayout->addWidget(xb2only, 1, 3);
-            QPushButton *CreateBlade= new QPushButton("Create Blade", this);
+            CreateBlade= new QPushButton("Create Blade", this);
             CreateBlade->setFixedWidth(200);
             taskGridLayout->addWidget(CreateBlade, 2, 3);
+            CreateBlade->setEnabled(false);
 
-            QPushButton *ReadSave = new QPushButton("Read Save", this);
+            ReadSave = new QPushButton("Read Save", this);
             ReadSave->setFixedWidth(200);
             taskGridLayout->addWidget(ReadSave, 3, 3);
+            ReadSave->setEnabled(false);
 
-            QPushButton *DecompressIraSave  = new QPushButton("Decompress Ira Save", this);
+            DecompressIraSave  = new QPushButton("Decompress Ira Save", this);
             DecompressIraSave->setFixedWidth(200);
             taskGridLayout->addWidget(DecompressIraSave, 4, 3);
+            DecompressIraSave->setEnabled(false);
 
-        QLabel *xc1DEonly = new QLabel("Xenoblade DE only", this);
-        taskGridLayout->addWidget(xc1DEonly, 1, 4);
-            QPushButton *GenerateDropTables = new QPushButton("Generate Drop Tables", this);
+        QLabel *xb1DEonly = new QLabel("Xenoblade DE only", this);
+        taskGridLayout->addWidget(xb1DEonly, 1, 4);
+            GenerateDropTables = new QPushButton("Generate Drop Tables", this);
             GenerateDropTables->setFixedWidth(200);
             taskGridLayout->addWidget(GenerateDropTables, 2, 4);
             GenerateDropTables->setEnabled(false);
 
-        //si xenoblade 2 n'est pas choisi alors griser boutons tasks suivants : CreateBlade | ReadSave | DecompressIraSave
-        // et pour XC1DE :  GenerateDropTables
-            //->setEnabled(false);
-
+        /*
+        QString commandUser;
+        QString commandGame;
+        QString commandTask;
+        QString commandArchive;
+        QString commandBDAT;
+        QString commandInput;
+        QString commandOutput;
+        */
 
     stack->addWidget(task);
 
-    stack->setCurrentIndex(1);
+    stack->setCurrentIndex(0);
 }
 MainWindow::~MainWindow(){}
--- mainwindow.h
+++ mainwindow.h
@@ -9,6 +9,7 @@
 #include <QGridLayout>
 #include <QPushButton>
 #include <QLabel>
+#include <QDebug>
 
 class MainWindow : public QMainWindow
 {
@@ -20,11 +21,36 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     QWidget* getCentralWidget() const {return centralWidget;}
+    QString commandUser;
+    QString commandGame;
+    QString commandTask;
+    QString commandArchive;
+    QString commandBDAT;
+    QString commandInput;
+    QString commandOutput;
 
 private:
     QWidget *centralWidget;
     QStackedLayout *stack;
     //QGridLayout *gridLayout;
+    QPushButton *xb1;
+    QPushButton *xb2;
+    QPushButton *xb3;
+    QPushButton *xbx;
+    QPushButton *ExtractArchive;
+    QPushButton *ReplaceArchive;
+    QPushButton *DecryptBdat;
+    QPushButton *BdatCodeGen;
+    QPushButton *Bdat2Html;
+    QPushButton *Bdat2Json;
+    QPushButton *Bdat2Psql;
+    QPushButton *GenerateData;
+    QPushButton *DescrambleScript;
+    QPushButton *ExtractWilay;
+    QPushButton *CreateBlade;
+    QPushButton *ReadSave;
+    QPushButton *DecompressIraSave;
+    QPushButton *GenerateDropTables;
 
 };
 #endif
